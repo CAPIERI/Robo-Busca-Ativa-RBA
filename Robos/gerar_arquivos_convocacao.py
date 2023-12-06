6a1931fd9450e1ca5bb6bdaf86b6ba7a6db45cbe
igorfcfs <igorfcfs@gmail.com>
2023-12-06 19:09:23 -0300

    RBA 2.2 - Deadline, Mensagem Deadline, Enviar para o email e atualizações

--- Robos/gerar_arquivos_convocacao.py
+++ Robos/gerar_arquivos_convocacao.py
@@ -49,7 +49,7 @@ def unir_convocacoes():
     arquivos_listados = [arquivo for arquivo in arquivos if arquivo.endswith('.docx')]
 
     doc_inicial = Path(diretorio / f'{arquivos_listados[0]}')
-    doc_todas_conv = diretorio.parent / "Todas_convocações.docx"
+    doc_todas_conv = diretorio.parent / "ConvocacaoParaCompensarFaltas.docx"
     shutil.copyfile(doc_inicial, doc_todas_conv)
 
     for arquivo in arquivos_listados[1:]:
@@ -63,22 +63,7 @@ def unir_convocacoes():
 
 unir_convocacoes()
 
-def executar_script_converter_convocacao_para_pdf():
-    # Obter o diretório do arquivo em execução
-    diretorio_atual = os.path.dirname(os.path.abspath(__file__))
-
-    # Nome do arquivo a ser executado (neste caso, na mesma pasta)
-    caminho_segundo_script = "converter_convocacao_para_pdf.py"
-
-    # Caminho completo para o segundo script
-    caminho_completo = os.path.join(diretorio_atual, caminho_segundo_script)
-
-    # Executar o segundo script
-    subprocess.call(["python", caminho_completo])
-
-# Execute o segundo script
-executar_script_converter_convocacao_para_pdf()
 
 def converter_para_pdf():
-    convert(Path(__file__).parent.parent / "ArquivosGerados/Todas_convocações.docx", Path(__file__).parent.parent / "ArquivosGerados")
+    convert(Path(__file__).parent.parent / "ArquivosGerados/ConvocacaoParaCompensarFaltas.docx", Path(__file__).parent.parent / "ArquivosGerados")
 converter_para_pdf()

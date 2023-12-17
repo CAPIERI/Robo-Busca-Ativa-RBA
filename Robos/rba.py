c35dbbb75064d40fceeae52fa63de8664add9a4b
igorfcfs <igorfcfs@gmail.com>
2023-12-17 19:59:09 -0300

    RBA v2.5 - Atualizacao da Tela

--- Robos/rba.py
+++ Robos/rba.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import tkinter as tk
 from tkinter import messagebox
 
-deadline = datetime(2023, 12, 31, 23, 59, 59)
+deadline = datetime(2024, 12, 31, 23, 59, 59)
 
 if datetime.today() <= deadline:
     diretorio_padrao = "../Planilias"
@@ -76,7 +76,7 @@ if datetime.today() <= deadline:
 
             print(f'Documento gerado e salvo em {document_path_arquivos_gerados}')
         substituir_documento()
-
+        
     def janela_Modelo_Conv():
         def salvar_informacoes():
             global departamento, regiao_escola, rua, numero_endereco, bairro, cidade, estado, cep, telefone, email
@@ -94,59 +94,70 @@ if datetime.today() <= deadline:
             janela_modelo_convocacao.destroy()
 
         janela_modelo_convocacao = ctk.CTkToplevel()
+        janela_modelo_convocacao.resizable(False, False)
         janela_modelo_convocacao.geometry("800x500")
         janela_modelo_convocacao.title("Modelo Convocação")
+        
+        # Define o ícone da janela
+        # janela_modelo_convocacao.iconbitmap("caminho/do/arquivo.ico")
 
-        label_modelo_convocacao = ctk.CTkLabel(janela_modelo_convocacao, text="Por Favor preencha as informações da sua Escola:")
-        label_modelo_convocacao.pack()
+        label_modelo_convocacao = ctk.CTkLabel(janela_modelo_convocacao, text="Por favor, preencha as informações da sua Escola:")
+        label_modelo_convocacao.pack(pady=10)
 
         entry_departamento_escola = ctk.CTkEntry(janela_modelo_convocacao, placeholder_text="Departamento da Escola")
-        entry_departamento_escola.pack()
+        entry_departamento_escola.pack(padx=20, pady=5)
 
         entry_regiao_escola = ctk.CTkEntry(janela_modelo_convocacao, placeholder_text="Região da Escola")
-        entry_regiao_escola.pack()
+        entry_regiao_escola.pack(pady=5)
 
         entry_rua = ctk.CTkEntry(janela_modelo_convocacao, placeholder_text="Rua")
-        entry_rua.pack()
+        entry_rua.pack(pady=5)
 
         entry_numero_endereco = ctk.CTkEntry(janela_modelo_convocacao, placeholder_text="Número do Endereço")
-        entry_numero_endereco.pack()
+        entry_numero_endereco.pack(pady=5)
 
         entry_bairro = ctk.CTkEntry(janela_modelo_convocacao, placeholder_text="Bairro")
-        entry_bairro.pack()
+        entry_bairro.pack(pady=5)
 
         entry_cidade = ctk.CTkEntry(janela_modelo_convocacao, placeholder_text="Cidade")
-        entry_cidade.pack()
+        entry_cidade.pack(pady=5)
 
         entry_estado = ctk.CTkEntry(janela_modelo_convocacao, placeholder_text="Estado")
-        entry_estado.pack()
+        entry_estado.pack(pady=5)
 
         entry_cep = ctk.CTkEntry(janela_modelo_convocacao, placeholder_text="CEP")
-        entry_cep.pack()
+        entry_cep.pack(pady=5)
 
         entry_telefone = ctk.CTkEntry(janela_modelo_convocacao, placeholder_text="Telefone")
-        entry_telefone.pack()
+        entry_telefone.pack(pady=5)
 
         entry_email = ctk.CTkEntry(janela_modelo_convocacao, placeholder_text="Email")
-        entry_email.pack()
+        entry_email.pack(pady=5)
 
         botao_salvar = ctk.CTkButton(janela_modelo_convocacao, text="Salvar", command=salvar_informacoes)
-        botao_salvar.pack()
+        botao_salvar.pack(pady=20)
+        
+        # O metodo grab_set() garante que a janela de modelo_convocacao apareça acima da janela principal, ja o lift() faz o oposto
+        janela_modelo_convocacao.grab_set()
 
     janela_princ = ctk.CTk()
+    janela_princ.resizable(False, False)
     janela_princ.geometry("800x500")
     janela_princ.minsize(width=800, height=500)
     janela_princ.maxsize(width=800, height=500)
-    janela_princ.title("Busca Ativa")
+    janela_princ.title("Robô Busca Ativa - RBA")
+    
+    # Define o ícone da janela
+    # janela_modelo_convocacao.iconbitmap("caminho/do/arquivo.ico")
 
-    label_busca_ativa = ctk.CTkLabel(janela_princ, width=40, height=40, text="Busca Ativa")
-    label_busca_ativa.pack()
+    label_busca_ativa = ctk.CTkLabel(janela_princ, text="Robô Busca Ativa")
+    label_busca_ativa.pack(pady=10)
 
-    botao_modelo_conv = ctk.CTkButton(janela_princ, text="Criar Modelo De Convocação", command=janela_Modelo_Conv)
-    botao_modelo_conv.pack()
+    botao_modelo_conv = ctk.CTkButton(janela_princ, text="Criar Modelo de Convocação", command=janela_Modelo_Conv)
+    botao_modelo_conv.pack(pady=15)
 
     botao_rodar_robo = ctk.CTkButton(janela_princ, text="Rodar", command=executar_tudo)
-    botao_rodar_robo.pack()
+    botao_rodar_robo.pack(pady=30)
 
     diretorio_padrao = ctk.CTk
 
